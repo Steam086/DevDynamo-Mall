78a5d06c0dfb97ddca328d1a98b8b78a5a1e17a9
jjr <jjrcsu@163.com>
2025-01-30 16:11:53 +0800

    Fixed maven and dependency problems

--- auth-service/src/main/java/com/devdynamo/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/com/devdynamo/auth/AuthServiceApplication.java
@@ -3,7 +3,7 @@ package com.devdynamo.auth;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
+
 
 @SpringBootApplication
 @EnableDubbo
