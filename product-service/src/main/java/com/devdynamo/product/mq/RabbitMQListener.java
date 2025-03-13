db0050d0e91c7852886fd9ed599ca48813c2b9e5
jjr <jjrcsu@163.com>
2025-03-14 00:26:13 +0800

    新增创建商品逻辑，RabbitMQ Listener逻辑待实现

---
+++ product-service/src/main/java/com/devdynamo/product/mq/RabbitMQListener.java
@@ -0,0 +1,35 @@
+package com.devdynamo.product.mq;
+
+import com.devdynamo.product.elasticsearch.repository.EsProductRepository;
+import com.devdynamo.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RabbitMQListener {
+
+    private static final String QUEUE_NAME = "product-queue";
+
+    private final ProductRepository productRepository;
+
+    private final EsProductRepository esProductRepository;
+
+    /**
+     * 将数据写入到Elasticsearch的逻辑
+     * @param message
+     */
+    @RabbitListener(queues = QUEUE_NAME)
+    public void listen(final String message) {
+        log.info("Received product ID from RabbitMQ: {}", message);
+
+        // TODO 查找数据库
+
+
+        //TODO 转存到 Elasticsearch
+
+    }
+}
--- product-service/src/main/java/com/devdynamo/product/service/ProductServiceImpl.java
+++ product-service/src/main/java/com/devdynamo/product/service/ProductServiceImpl.java
@@ -2,18 +2,52 @@ package com.devdynamo.product.service;
 
 
 import com.devdynamo.entity.product.Product;
+import com.devdynamo.product.elasticsearch.document.EsProduct;
+import com.devdynamo.product.elasticsearch.repository.EsProductRepository;
+import com.devdynamo.product.repository.ProductRepository;
 import com.devdynamo.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 //@DubboService
 public class ProductServiceImpl implements ProductService {
 
+    private final ProductRepository productRepository;
+
+    private final EsProductRepository esProductRepository;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    private static final String PRODUCT_QUEUE = "product-queue";
+
+    /**
+     * 方案一： 在数据存到数据库之后向MQ发送待保存的product的id,
+     * MQ消费者接受到id之后查数据库并将内容同步到Elasticsearch
+     * @param product 待创建的商品信息
+     * @return 创建成功的Product
+     */
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public Product create(Product product) {
-        return null;
+        Product savedProduct;
+        try{
+            savedProduct = productRepository.save(product);
+            Long id = savedProduct.getId();
+            // 将id发送到MQ,消费者处理同步逻辑
+            rabbitTemplate.convertAndSend(PRODUCT_QUEUE, id);
+        }
+        catch(Exception e){
+            log.error(e.getMessage());
+        }
+        return product;
     }
 
     @Override
